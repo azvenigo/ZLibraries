cf811a8f8d1d40d3972badbb1577b1d79c144cd6
Alex Zvenigorodsky <3175403+azvenigo@users.noreply.github.com>
2022-05-08 11:51:08 -0700

    Fixing DumpCout formatting for some byte counts.

--- Common/helpers/LoggingHelpers.h
+++ Common/helpers/LoggingHelpers.h
@@ -13,23 +13,33 @@ inline std::string HexValueOut(uint32_t nVal, bool bIncludeDecimal = true)
     return std::string(buf);
 }
 
+inline std::string HexValueOut(uint64_t nVal, bool bIncludeDecimal = true)
+{
+    char buf[64];
+    if (bIncludeDecimal)
+        sprintf_s(buf, "0x%016llx (%lld)", nVal, nVal);
+    else
+        sprintf_s(buf, "0x%016llx", nVal);
+    return std::string(buf);
+}
+
 inline void DumpMemoryToCout(uint8_t* pBuf, uint32_t nBytes, uint32_t nBaseMemoryOffset = 0, uint32_t nColumns = 32)
 {
     char byteToAscii[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
-    int32_t nBytesOnLine = 0;
+    uint32_t nBytesOnLine = 0;
     uint8_t* pWalker = pBuf;
     std::string sAscii;
     while (pWalker < pBuf + nBytes)
     {
-        if (nBytesOnLine % nColumns == 0)    // 32 bytes per line
+        if (nBytesOnLine % nColumns == 0)    
         {
             // if we've reached a line end and we've accumulated ascii text
             if (!sAscii.empty())
                 std::cout << " |" << sAscii << "|";
 
-            std::cout << "\n" << HexValueOut(nBaseMemoryOffset + (pWalker - pBuf), false) << ": ";
             nBytesOnLine = 0;
+            std::cout << "\n" << HexValueOut((uint32_t)(nBaseMemoryOffset + (pWalker - pBuf)), false) << ": ";
             sAscii.clear();
         }
         else if (nBytesOnLine % 4 == 0)  // extra space every 4 bytes
@@ -54,12 +64,13 @@ inline void DumpMemoryToCout(uint8_t* pBuf, uint32_t nBytes, uint32_t nBaseMemor
     if (!sAscii.empty())
     {
         // add spaces for any missing chars
-        while (nBytesOnLine++ < nColumns)
+        while (nBytesOnLine < nColumns)
         {
             sAscii += ' ';
             std::cout << "   ";
-            if (nBytesOnLine % 4 == 0)
+            if (nBytesOnLine % 4 == 0 && nBytesOnLine < nColumns)
                 std::cout << " ";
+            nBytesOnLine++;
         }
         std::cout << " |" << sAscii << "|\n";
     }
